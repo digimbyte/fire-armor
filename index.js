9072be6870c44afc921cd298f6364b8b5107461a
DharmarajX24 <63334359+DharmarajX24@users.noreply.github.com>
2021-02-20 21:05:12 +0530

    Added new node modules

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express")
 const admin = require("firebase-admin")
 
+const cors = require("cors")
+const bodyParser = require("body-parser")
+
 const app = express()
+
+app.use(cors({origin: true, credentials: true}))
+app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.get("*", function (req, res) {
     return res.sendStatus(200)
